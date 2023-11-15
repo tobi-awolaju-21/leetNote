284d64e3afbbcc44765f1d7e31b2dcf5baf8ffab
imagine tobi <tobiawolaju21@gmail.com>
2023-11-15 19:13:41 +0100

    new project

---
+++ App.js
@@ -0,0 +1,3 @@
+//load upcoming space into the peopelSpaces div on index.html
+//load personalized people course based on users info, else bring up login dialog on index.
+// change txt peoople notes to username note
