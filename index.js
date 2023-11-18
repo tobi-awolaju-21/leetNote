d22ed8f78c14e6c909a6a12d9106e99b5d8c5de8
imagine tobi <tobiawolaju21@gmail.com>
2023-11-18 01:46:02 +0100

    rest

--- index.js
+++ index.js
@@ -29,12 +29,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-if (document.documentElement.requestFullscreen) {
-    document.documentElement.requestFullscreen();
-  } else if (document.documentElement.mozRequestFullScreen) {
-    document.documentElement.mozRequestFullScreen();
-  } else if (document.documentElement.webkitRequestFullscreen) {
-    document.documentElement.webkitRequestFullscreen();
-  } else if (document.documentElement.msRequestFullscreen) {
-    document.documentElement.msRequestFullscreen();
-  }
---
+++ pages/js/global.js
@@ -0,0 +1,9 @@
+if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.mozRequestFullScreen) {
+    document.documentElement.mozRequestFullScreen();
+  } else if (document.documentElement.webkitRequestFullscreen) {
+    document.documentElement.webkitRequestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) {
+    document.documentElement.msRequestFullscreen();
+  }
