f003c4dc2ceb334f0f2c151d97e78e11e6d63fe6
imagine tobi <tobiawolaju21@gmail.com>
2023-11-18 01:46:15 +0100

    okay

--- pages/js/global.js
+++
@@ -1,9 +0,0 @@
-if (document.documentElement.requestFullscreen) {
-    document.documentElement.requestFullscreen();
-  } else if (document.documentElement.mozRequestFullScreen) {
-    document.documentElement.mozRequestFullScreen();
-  } else if (document.documentElement.webkitRequestFullscreen) {
-    document.documentElement.webkitRequestFullscreen();
-  } else if (document.documentElement.msRequestFullscreen) {
-    document.documentElement.msRequestFullscreen();
-  }
