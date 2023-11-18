81a220d4363e936b13de6c3c49ebbc14dc8d23ff
imagine tobi <tobiawolaju21@gmail.com>
2023-11-18 08:19:37 +0100

    rst

--- index.js
+++ index.js
@@ -1,49 +1,3 @@
-//load upcoming space into the peopelSpaces div on index.html
-//load personalized people course based on users info, else bring up login dialog on index.
-// change txt peoople notes to username note.
-
-//listen to note clicked after notes have been loaded 
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the element with the class 'scroll-item2'
-    var scrollItem = document.querySelector('.scroll-item2');
-
-    // Add a click event listener to the element
-    scrollItem.addEventListener('click', function() {
-        // Redirect to 'notes.html' when the element is clicked
-        window.location.href = './pages/notes.html';
-    }); 
-});
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the element with the class 'scroll-item2'
-    var scrollItem = document.querySelector('.scroll-item');
-
-    // Add a click event listener to the element
-    scrollItem.addEventListener('click', function() {
-        // Redirect to 'notes.html' when the element is clicked
-        window.location.href = './pages/leetspace.html';
-    }); 
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
    // Import the functions you need from the SDKs you need
@@ -66,10 +20,19 @@ document.addEventListener('DOMContentLoaded', function() {
    const auth = getAuth(app);
 
 
+      
 
-   document.querySelector('img').onclick = function() {
-    // Define the signInWithGoogle function here
+  document.addEventListener('DOMContentLoaded', function() {
+    // Get the element with the class 'scroll-item2'
+    var scrollItem = document.querySelector('img');
+  
+    // Add a click event listener to the element
+    scrollItem.addEventListener('click', function() {
+
+      
+    // Call the signInWithGoogle function
     function signInWithGoogle() {
+      console.log("trying")
       // Implement the Google sign-in logic here
       const googleProvider = new GoogleAuthProvider();
     const signInWithGoogle = async () => {
@@ -82,8 +45,47 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     };
     }
-  
-    // Call the signInWithGoogle function
-    signInWithGoogle();
-  };
-      
+    signInWithGoogle()
+
+
+
+
+
+
+
+
+
+
+    }); 
+  });
+
+
+
+  //load upcoming space into the peopelSpaces div on index.html
+//load personalized people course based on users info, else bring up login dialog on index.
+// change txt peoople notes to username note.
+
+//listen to note clicked after notes have been loaded 
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the element with the class 'scroll-item2'
+  var scrollItem = document.querySelector('.scroll-item2');
+
+  // Add a click event listener to the element
+  scrollItem.addEventListener('click', function() {
+      // Redirect to 'notes.html' when the element is clicked
+      window.location.href = './pages/notes.html';
+  }); 
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the element with the class 'scroll-item2'
+  var scrollItem = document.querySelector('.scroll-item');
+
+  // Add a click event listener to the element
+  scrollItem.addEventListener('click', function() {
+      // Redirect to 'notes.html' when the element is clicked
+      window.location.href = './pages/leetspace.html';
+  }); 
+});
+
