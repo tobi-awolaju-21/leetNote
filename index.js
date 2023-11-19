7a42fbfba45da40830ce4d9ecb73889fea29a585
imagine tobi <tobiawolaju21@gmail.com>
2023-11-19 14:38:02 +0100

    rest

--- index.js
+++ index.js
@@ -59,35 +59,6 @@
 
 
 
-  //load upcoming space into the peopelSpaces div on index.html
-//load personalized people course based on users info, else bring up login dialog on index.
-// change txt peoople notes to username note.
-
-//listen to note clicked after notes have been loaded 
-document.addEventListener('DOMContentLoaded', function() {
-  // Get the element with the class 'scroll-item2'
-  var scrollItem = document.querySelector('.scroll-item2');
-
-  // Add a click event listener to the element
-  scrollItem.addEventListener('click', function() {
-      // Redirect to 'notes.html' when the element is clicked
-      window.location.href = './pages/notes.html';
-  }); 
-});
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  // Get the element with the class 'scroll-item2'
-  var scrollItem = document.querySelector('.scroll-item');
-
-  // Add a click event listener to the element
-  scrollItem.addEventListener('click', function() {
-      // Redirect to 'notes.html' when the element is clicked
-      window.location.href = './pages/leetspace.html';
-  }); 
-});
-
-
  
 // load Spaces 
 // URL of the JSON data
@@ -202,12 +173,6 @@ fetch(jsonUrl)
   .catch(error => {
     console.error('Error fetching JSON:', error);
   });
-
-
-
-
-
-
 
 
 
