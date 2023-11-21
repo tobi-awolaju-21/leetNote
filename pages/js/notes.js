bb5a895d86503766568cd20d8a9cf2b9cc148f7b
imagine tobi <tobiawolaju21@gmail.com>
2023-11-21 01:51:03 +0100

    rest

--- pages/js/notes.js
+++ pages/js/notes.js
@@ -1,24 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the element with the class 'scroll-item2'
-    var scrollItem = document.querySelector('.notefab');
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the element with the class 'scroll-item2'
+  var scrollItem = document.querySelector(".notefab");
 
-    // Add a click event listener to the element
-    scrollItem.addEventListener('click', function() {
-        // Redirect to 'notes.html' when the element is clicked
-        window.location.href = './test.html';
-    }); 
+  // Add a click event listener to the element
+  scrollItem.addEventListener("click", function () {
+    // Redirect to 'notes.html' when the element is clicked
+    window.location.href = "./test.html";
+  });
 });
 
-
-
-
-
-
-
-
-
-
-  // load courses
+// load courses
 // URL of the JSON data = baseurl/ +surfix of webpage.json
 //http://127.0.0.1:5500/pages/leetspace.html?courseKey=AGE%20202
 
@@ -30,12 +21,13 @@ function getParameterValue(parameterName) {
   return urlSearchParams.get(parameterName);
 }
 // Get the value of the courseKey parameter
-const courseKey = getParameterValue('courseKey');
+const courseKey = getParameterValue("courseKey");
 // Log the value to the console (you can use it as needed)
 
-
-
-var jsonUrl = "https://leetnote-7cfce-default-rtdb.firebaseio.com/courses/" + courseKey + ".json"
+var jsonUrl =
+  "https://leetnote-7cfce-default-rtdb.firebaseio.com/courses/" +
+  courseKey +
+  ".json";
 
 console.log(jsonUrl);
 
@@ -44,7 +36,7 @@ var peopleCoursesElement = document.querySelector(".peopleCourses");
 
 // Fetch the JSON data
 fetch(jsonUrl)
-  .then(response => {
+  .then((response) => {
     // Check if the response status is OK (200)
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -52,44 +44,42 @@ fetch(jsonUrl)
     // Parse the JSON from the response
     return response.json();
   })
-  .then(jsonData => {
-// start of magic
+  .then((jsonData) => {
+    // start of magic
 
+    const metadataArray = JSON.parse(jsonData.metadata);
 
-
-
-
-
-const metadataArray = JSON.parse(jsonData.metadata);
-
-metadataArray.forEach(item => {
-    // Creating div for note
-    if (item.note) {
-        const noteDiv = document.createElement('div');
-        noteDiv.className = 'note-tile';
-        noteDiv.innerHTML = item.note;
-        peopleCoursesElement.appendChild(noteDiv);
-    }
-
-   
-
-
-    // Creating div for outline
-    if (item.outline) {
-        const outlineDiv = document.createElement('div');
-        outlineDiv.className = 'player';
+    metadataArray.forEach((item) => {
+      // Creating div for outline
+      if (item.outline) {
+        const outlineDiv = document.createElement("div");
+        outlineDiv.className = "note-tile";
         outlineDiv.innerHTML = item.outline;
         peopleCoursesElement.appendChild(outlineDiv);
-    }
+      }
 
-    
+     // Creating div for video
+if (item.video) {
+  const noteDiv = document.createElement("div");
+  noteDiv.className = "player";
+  noteDiv.innerHTML = `<iframe src="${item.video}" width="100%" height="100%" frameborder="0" allowfullscreen></iframe>`;
+  peopleCoursesElement.appendChild(noteDiv);
+}
 
-    // Creating divs for questions and solutions
-    if (item.question) {
+      // Creating div for note
+      if (item.note) {
+        const noteDiv = document.createElement("div");
+        noteDiv.className = "note-tile";
+        noteDiv.innerHTML = item.note;
+        peopleCoursesElement.appendChild(noteDiv);
+      }
+
+      // Creating divs for questions and solutions
+      if (item.question) {
         const questionsArray = JSON.parse(item.question);
-        questionsArray.forEach(questionItem => {
-            // Using the provided HTML snippet for questions
-            const questionTileHTML = `
+        questionsArray.forEach((questionItem) => {
+          // Using the provided HTML snippet for questions
+          const questionTileHTML = `
                 <div class="question-tile">
                     <p>${questionItem.question}</p>
                     <input class="submit-button" type="file" id="imageInput" accept="image/*" style="display: block;">
@@ -99,22 +89,13 @@ metadataArray.forEach(item => {
                 </div>
             `;
 
-            const questionDiv = document.createElement('div');
-            questionDiv.innerHTML += questionTileHTML;
-            peopleCoursesElement.appendChild(questionDiv);
+          const questionDiv = document.createElement("div");
+          questionDiv.innerHTML += questionTileHTML;
+          peopleCoursesElement.appendChild(questionDiv);
         });
-    }
-});
-
-
-
-
-//end of magic
-console.log("deyplay");
-
-   })
-  .catch(error => {
-    console.error('Error fetching JSON:', error);
+      }
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching JSON:", error);
   });
-
-
