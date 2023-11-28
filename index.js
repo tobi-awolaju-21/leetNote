4b3965c5bfcd7fae7e249f1e92dd94e13a8d0c98
imagine tobi <tobiawolaju21@gmail.com>
2023-11-28 04:00:19 +0100

    rest

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ function redirectToNotes(clickedCourseKey) {
   var encodedCourseKey = encodeURIComponent(clickedCourseKey);
   
   // add the email too 
+  const emailElement = document.getElementsByClassName('email');
   var email = emailElement.innerText;
   
   // Redirect to notes.html with the courseKey and email added to the URL
