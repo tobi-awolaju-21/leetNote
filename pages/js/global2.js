bdbb6cf6f799c88173924aa399b8cbdc3d69f12e
Tobi Awolaju <120692959+tobi-awolaju-21@users.noreply.github.com>
2023-12-03 11:58:30 +0100

    Update js

---
+++ pages/js/global2.js
@@ -0,0 +1,10 @@
+//check subscription status by extracting the parameter email from the current page url
+
+ApiEndPoint = "https://www.google.com"
+
+//send the parameter email via http post request to the url ApiEndPoint
+
+//await the response
+//if it returned true, console.log this is a subscriber
+//else console log not a subscriber and 
+redirect to profile page.
