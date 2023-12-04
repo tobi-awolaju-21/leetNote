7b90d115bcb8d5ae65ae0e2411bd4fc83a5a1aa5
imagine tobi <tobiawolaju21@gmail.com>
2023-12-04 01:40:32 +0100

    rest

--- pages/js/user.js
+++ pages/js/user.js
@@ -16,26 +16,62 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const database = getDatabase(firebaseApp);
 
-const existingUserData = {
-    "courses": "[{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"},{\"code\":\"MTS 201\",\"progress\":\"0%\"}]",
-    "logfile": "[{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"},{\"event\":\"joinedSpace\",\"timestamp\":\"36685578889\"}]",
-    "metadata": "{\"firstOnline\":\"628229393\",\"level\":\"200lv\",\"name\":\"firstuser\",\"lastOnline\":\"63839393\",\"id\":\"62c519101bc628\",\"department\":\"EEE\"}",
-    "username": "firstuser"
-};
+// Fetch data from the specified URL
+const url = "https://leetnote-7cfce-default-rtdb.firebaseio.com/users/firstuser.json";
 
-// Parse the existing user data
-const existingCourses = JSON.parse(existingUserData.courses);
-const existingLogfile = JSON.parse(existingUserData.logfile);
-const existingMetadata = JSON.parse(existingUserData.metadata);
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Use the loaded data
+    console.log("Loaded data:", data);
 
-// Update the Firebase Realtime Database with the existing user data
-const dataToUpdate = {
-    username: existingUserData.username,
-    metadata: existingMetadata,
-    courses: existingCourses,
-    logfile: existingLogfile
-};
+    // Now you can work with the loaded data, for example, update the Realtime Database
+    const firebaseConfig = {
+      apiKey: "AIzaSyB8-0G6-iS8Ep9fwOl6yBlGuUF0DxS0wdo",
+      authDomain: "leetnote-7cfce.firebaseapp.com",
+      projectId: "leetnote-7cfce",
+      storageBucket: "leetnote-7cfce.appspot.com",
+      messagingSenderId: "43966757694",
+      appId: "1:43966757694:web:f80fcd4da26cdbc1a2ca6f",
+      measurementId: "G-42H2T6F7DE"
+    };
 
-// Update the Firebase Realtime Database
-const databaseRef = ref(database, `users/${existingUserData.username}`);
-update(databaseRef, dataToUpdate);
+    const firebaseApp = initializeApp(firebaseConfig);
+    const database = getDatabase(firebaseApp);
+
+    // Assume existingUserData is the loaded data
+    const existingUserData = data;
+
+    // Parse the existing user data
+    var existingCourses = JSON.parse(existingUserData.courses);
+    var existingLogfile = JSON.parse(existingUserData.logfile);
+    var existingMetadata = JSON.parse(existingUserData.metadata);
+
+
+    var existingLogfile = [
+        {"event": "joinedSpace", "timestamp": "36685578889"},
+      ];
+    var logfile = JSON.stringify(existingLogfile);
+
+    // Update the Firebase Realtime Database with the existing user data
+    const dataToUpdate = {
+      username: existingUserData.username,
+      metadata: JSON.stringify(existingMetadata),
+      courses: JSON.stringify(existingCourses),
+      logfile: logfile
+    };
+
+    console.log(JSON.stringify(logfile));
+    
+    // Update the Firebase Realtime Database
+    const databaseRef = ref(database, `users/${existingUserData.username}`);
+    update(databaseRef, dataToUpdate);
+  })
+  .catch(error => {
+    console.error("Error fetching data:", error);
+  });
